8d9ecdb7f6b296cf1f59b6b366c82a19b4980ba4
Timothy Mc Alister <timothy@mcalister.ch>
2017-09-16 10:58:29 +0200

    added logo and started EthWrapper

--- hacktest/eth.js
+++ hacktest/eth.js
@@ -1,28 +1,53 @@
-var Web3 = require('./node_modules/Web3');
+const Web3 = require('web3');
+const abi = require('human-standard-token-abi') // (erc20 abi) -> replace with securether abi
+const net = require('net')
 
-if (typeof web3 !== 'undefined') {
-  web3 = new Web3(web3.currentProvider);
-} else {
-  // set the provider you want from Web3.providers
-  //web3 = new Web3(new Web3.providers.HttpProvider("http://172.30.0.48:8545"));
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+class EthWrapper {
+    constructor(erc20contract){
+        // initialise IpcProvider from local file
+        this.web3 = new Web3(new Web3.providers.IpcProvider('/Users/timothy/Library/Ethereum/rinkeby/geth.ipc',net));
+        // instantiate contract from abi and contract address 
+        this.erc20 = new this.web3.eth.Contract(erc20contract.abi, erc20contract.address);
+    }
+
+    readKeyFile(password){
+        // read encrypted key file
+    }
+
+    createKeyPair(salt){
+        const account = this.web3.eth.accounts.create(salt);
+        console.log('Created account:');
+        console.log('Account: '+ account.address);
+        console.log('Private Key: ' + account.privateKey);
+        return({address: account.address,privateKey: account.privateKey});
+    }
+
+    addAccountToWallet(privateKey){
+        this.web3.eth.accounts.wallet.add(myAccount.privateKey);
+    }
+
+    getRecovetherBalanceOf(address){
+        this.erc20.methods.balanceOf('9f6294a019a2c5ee3e0ad46b7557ad4926cf8723').call().then((balance)=>{
+            return(balance);
+        });
+    }
+
+    // subscribe to contract event
+    on(eventName,callback){
+        // Perhaps intercept first?
+        this.web3.eth.subscribe(eventName,{},(error,result)=> {
+            console.log(result);
+        });
+    }
 }
 
-var maxlevels = 10;
+const erc20contract = {abi: abi, address: '9f6294a019a2c5ee3e0ad46b7557ad4926cf8723'};
+const ethWrapper = new EthWrapper(erc20contract);
 
-var getValue = function(n){
-    if (n > maxlevels)
-        return;
-    web3.eth.getStorageAt('a72b95cbe173840cb933b5225523c3b1b6d0f68e', n,(error,result) => {
-        if(!error)
-        {
-            console.log(result)
-            getValue(n+1);
-        }
-    else
-        console.error(error);
-    })
-}
+console.log(ethWrapper.createKeyPair('entropyyyyyyyyyyy'))
 
-getValue(0);
+
+
+
 
